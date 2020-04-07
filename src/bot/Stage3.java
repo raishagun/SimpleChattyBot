33e2d4117c302c91b836f758edafea6ac0840c21
raishagun <shagun.gla_cs18@gla.ac.in>
2020-04-07 23:06:38 +0530

    task description of third Stage

--- src/bot/Stage3.java
+++ src/bot/Stage3.java
@@ -1,5 +1,32 @@
 package bot;
+/*
+* Description
+At this stage, you will program the bot to count from 0 to any positive number you entered.
+
+Here is an example of a dialogue with the new version of the bot. Input lines are started with ">" symbol.
+
+Hello! My name is Aid.
+I was created in 2018.
+Please, remind me your name.
+> Max​​​​​​​
+What a great name you have, Max!
+Let me guess your age.
+Say me remainders of dividing your age by 3, 5 and 7.
+> 1 2 1
+Your age is 22; that's a good time to start programming!
+Now I will prove to you that I can count to any number you want.
+> 5
+0!
+1!
+2!
+3!
+4!
+5!
+Completed, have a nice day!
+Note, each number starts with a new line, and after a number, the bot should print the exclamation mark. Do not output any extra characters in these lines with numbers.*/
 
 public class Stage3 {
+    public static void main(String[] args) {
 
+    }
 }
