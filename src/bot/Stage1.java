6be6fd0367cd5318a97ad77666d3cf3e83f90b29
raishagun <shagun.gla_cs18@gla.ac.in>
2020-04-07 22:40:43 +0530

    description of the task

--- src/bot/Stage1.java
+++ src/bot/Stage1.java
@@ -1,4 +1,28 @@
+
+/*
+* Description
+Digital personal assistants help people to drive cars, plan their day, buy something online. In a sense, they are simplified versions of artificial intelligence with whom you can talk.
+
+In this project, you will develop step by step a simple bot which will help you to study programming.
+
+For the first stage, you will write a bot who displays a greeting, its name and the date of its creation.
+
+Your program must print the following lines:
+
+Hello! My name is {botName}.
+I was created in {birthYear}.
+Instead of {botName} print any name you choose and replace {birthYear} with the current year (four digits), ex:
+
+Hello! My name is Aid.
+I was created in 2018.
+You can change the text if you need but print exactly two lines.
+
+Next, we will use Aid and 2018 as your assistant's name and its birth year, but you can change it if you want*/
+
 package bot;
 
 public class Stage1 {
+    public static void main(String[] args) {
+
+    }
 }
