ace3c04cb5b2ed54163050bf17828181b6323b29
raishagun <shagun.gla_cs18@gla.ac.in>
2020-04-07 22:48:14 +0530

    read a name from user

--- src/bot/Stage2.java
+++ src/bot/Stage2.java
@@ -37,5 +37,10 @@ import java.util.Scanner;
 public class Stage2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+        // reading a name
+        String name = scanner.nextLine();
     }
 }
